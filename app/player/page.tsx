8ca15a702e97e1802264345d0706bef9d09f7761
Vasya1988 <myhappy1988@gmail.com>
2023-09-22 15:28:37 +0300

    third update

--- app/player/page.tsx
+++ app/player/page.tsx
@@ -8,39 +8,70 @@ import Audio from '@/components/audio/Audio'
 import { useRef, useEffect, useState } from 'react'
 
 const Player = () => {
+
     interface Track {
         path: string,
         art: string
     }
-    const [currentTrack, setCurrentTrack] = useState<Track | null>(null)
-    const [playPauseIcons, setPlayPauseIcons] = useState(true)
-    
+
+    const 
+        [currentTrack, setCurrentTrack] = useState<Track | null>(null),
+        [playPauseIcons, setPlayPauseIcons] = useState(true),
+        [currentTime, setCurrentTime] = useState<number | null>(null),
+        [duration, setDuration] = useState(0);
+        
     useEffect(() => {
         const randomTrack = Math.floor(Math.random() * PlayLIst().length)
         const playTrack = PlayLIst()[randomTrack]
         setCurrentTrack(playTrack)
-        console.log(playTrack.artist)
 
+        // Function for time line/ progrees
+        const handleTimeUpdate = () => {
+            audioRef.current?.currentTime && setCurrentTime(audioRef.current.currentTime)
+        }
+        audioRef.current?.addEventListener('timeupdate', handleTimeUpdate)
+
+        return () => {
+            audioRef.current?.removeEventListener('timeupdate', handleTimeUpdate)
+        }
     }, [])
+
+    interface ProgressLineEvent {
+        target: {value: string}
+
+    }
+    const handleProgressLine = (event: ProgressLineEvent) => {
+
+        // Get value of line
+        const value = event.target.value
+        // Calculate new time
+        const newTime = (Number(value) / 100) * duration
+
+        // Set new time into audioRef
+        audioRef.current?.currentTime = newTime
+    }
 
     const audioRef = useRef<HTMLAudioElement | null>(null)
     
+    // Start play or pause, and change the icons
     const playSong = () => {
         audioRef.current && audioRef.current.paused 
             ? (audioRef.current.play(), setPlayPauseIcons(false)) 
             : audioRef.current 
             ? (audioRef.current.pause(), setPlayPauseIcons(true)) 
             : null
-    } 
+    }
+
     const track = {path: currentTrack?.path}
+    console.log(currentTime)
     return (
         <div className={styles.Player}>
             <div className={styles['album-art']}>
                 <img src={currentTrack?.art} alt="" />
             </div>
-            <Audio track={currentTrack?.path} audioRef={audioRef}/>
+            <Audio  track={currentTrack?.path} audioRef={audioRef}/>
             <div className={styles.songLine}>
-                <input id={styles.Progress} type="range"/>
+                <input min='0' value='0' id={styles.Progress} type="range"/>
             </div>
             <nav className={styles['player-nav']}>
                 <PreviousButon />
