35bfdc7c685cd3513e28d46d8b712e55a5ed7f17
Vasya1988 <myhappy1988@gmail.com>
2023-09-29 15:50:53 +0300

    fifth update

--- app/player/page.tsx
+++ app/player/page.tsx
@@ -6,6 +6,8 @@ import NextButon from '@/components/buttons/NextButton'
 import PlayLIst from '../Play-list'
 import Audio from '@/components/audio/Audio'
 import { useRef, useEffect, useState } from 'react'
+import React from 'react'
+import { start } from 'repl'
 
 const Player = () => {
 
@@ -18,7 +20,10 @@ const Player = () => {
         [currentTrack, setCurrentTrack] = useState<Track | null>(null),
         [playPauseIcons, setPlayPauseIcons] = useState(true),
         [currentTime, setCurrentTime] = useState<number | null>(null),
-        [duration, setDuration] = useState(0);
+        [duration, setDuration] = useState(0),
+        [fullTime, setFullTime] = useState({min: '', seconds: ''}),
+        audioRef = useRef<HTMLAudioElement | null>(null),
+        [currentSeconds, setCurrentSeconds] = useState(0);
         
     useEffect(() => {
         const randomTrack = Math.floor(Math.random() * PlayLIst().length)
@@ -31,25 +36,51 @@ const Player = () => {
         }
         audioRef.current?.addEventListener('timeupdate', handleTimeUpdate)
 
-        
+        audioRef.current?.addEventListener('loadedmetadata', (event: Event) => {
+            const audioEvent = event.target as HTMLAudioElement
+            // console.log(audioEvent.duration)
+            getFullTimeSong(audioEvent.duration)
+        })
 
         return () => {
             audioRef.current?.removeEventListener('timeupdate', handleTimeUpdate)
         }
     }, [])
 
+    // Find out minutes and seconds of song
+    const getFullTimeSong = (time: number) => {
+
+        let minutes: string, seconds: string;
+
+        minutes = String(Math.floor((time / 60) % 60))
+        seconds = String(Math.floor(time % 60) < 10 ? `0${Math.floor(time % 60)}` : Math.floor(time % 60))
+        setFullTime({min: minutes, seconds: seconds})
+
+        // console.log('minutes: ', minutes, 'seconds: ', seconds)
+    }
+
+    const getCurrentSeconds = () => {
+
+        const startTime = setTimeout(() => {
+            let seconds
+            audioRef.current?.paused === false 
+                ? (seconds = currentSeconds + 1,
+                setCurrentSeconds(seconds), 
+                console.log(currentSeconds))
+                : false
+        }, 1000)
+    }
+    getCurrentSeconds()
+    
     interface ProgressLineEvent {
         target: {value: string}
 
     }
-
     // Calculate new time
-    const [newTime, setNewTime] = useState(0)
-
     const handleProgressLine = (event: ProgressLineEvent) => {
         
         console.log('input time ', Number(event.target.value))
-        console.log('audio time ', audioRef.current?.currentTime)
+        console.log('audio time ', audioRef.current?.duration)
         
         audioRef.current && (
             setCurrentTime(Number(event.target.value)),
@@ -58,15 +89,13 @@ const Player = () => {
         )
 
     }
-
-    const audioRef = useRef<HTMLAudioElement | null>(null)
     
     // Start play or pause, and change the icons
     const playSong = () => {
         audioRef.current && audioRef.current.paused 
             ? (audioRef.current.play(), setPlayPauseIcons(false)) 
             : audioRef.current 
-            ? (audioRef.current.pause(), setPlayPauseIcons(true)) 
+            ? (audioRef.current.pause(), setPlayPauseIcons(true), getCurrentSeconds()) 
             : null
     }
 
@@ -80,8 +109,8 @@ const Player = () => {
             <Audio  track={currentTrack?.path} audioRef={audioRef}/>
             <div className={styles.songLine}>
                 <div className={styles.Time}>
-                    <span className={styles.TimeStart}>{'0'}</span>
-                    <span className={styles.TimeStop}>{`${2}:${55}`}</span>
+                    <span className={styles.TimeStart}>{`0:0${currentSeconds}`}</span>
+                    <span className={styles.TimeStop}>{`${fullTime.min}:${fullTime.seconds}`}</span>
                 </div>
                 <input name='my input' min='0' max={duration} value={String(currentTime)} onChange={(event) => handleProgressLine(event)} id={styles.Progress} type="range"/>
             </div>
